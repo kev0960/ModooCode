379b581d723433faef44d4f68fb8d17d4d8cff75
Jaebum Lee <kev0960@gmail.com>
2018-08-30 02:08:01 -0700

    Setting some limits on compiler process

--- remo-code/src/remote_execute.cc
+++ remo-code/src/remote_execute.cc
@@ -1,3 +1,8 @@
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 #include <condition_variable>
 #include <mutex>
 #include <queue>
@@ -7,7 +12,20 @@
 #include <vector>
 
 using std::string;
+constexpr int kOneMb = 1024 * 1024;
+static char kGppName[] = "g++";
+static char kGppReadFromStdin[] = "-x";
+static char kGppLanguage[] = "c++";
+static char kDash[] = "-";
+static char kGppOutputFlag[] = "-o";
+static char kGppOutputFile[] = "./tmp/";
 
+// Instructions
+//
+// 1) Mount tmp directory as tmpfs;
+// sudo mount -t tmpfs -o size=16m tmpfs ./tmp
+//
+//
 namespace remo_code {
 class RemoteExecuter {
  private:
@@ -27,6 +45,58 @@ class RemoteExecuter {
   RemoteExecuter();
   void CodeExecutionThread();
 };
+
+string RemoteExecuter::SyncExecute(const string& code) {
+  int pipe_p2c[2];
+  if (pipe(pipe_p2c) != 0) {
+    return "Pipe is broken :(";
+  }
+
+  auto pid = fork();
+  if (pid < 0) {
+    // Fork error :(
+    return "Something went wrong :(";
+  } else if (pid == 0) {
+    // We are in the child process.
+
+    // Close write end and link STDIN through the pipe.
+    close(pipe_p2c[1]);
+    dup2(pipe_p2c[0], STDIN_FILENO);
+    close(pipe_p2c[0]);
+
+    // Restrict the amount of CPU time for the compilation.
+    const struct rlimit cpu_limit = {10, 10};
+    setrlimit(RLIMIT_CPU, &cpu_limit);
+
+    // Restrict the virtual memory size.
+    const struct rlimit mem_limit = {128 * kOneMb, 128 * kOneMb};
+    setrlimit(RLIMIT_AS, &mem_limit);
+
+    // Restrict the output file size.
+    const struct rlimit fs_limit = {2 * kOneMb, 2 * kOneMb};
+    setrlimit(RLIMIT_FSIZE, &fs_limit);
+
+    // Since we are just running a compiler, those restrictions above are good
+    // enough.
+    char* gpp_argv[] = {kGppName, kGppReadFromStdin, kGppLanguage,
+                        kDash,    kGppOutputFlag,    kGppOutputFile};
+    char* env[] = {NULL};
+    int ret = execve("/usr/bin/g++", gpp_argv, env);
+    return std::to_string(ret);
+  } else {
+    // We are in the parent process.
+    close(pipe_p2c[0]);
+    // Send the code through the pipe.
+    write(pipe_p2c[1], code.c_str(), code.size());
+    close(pipe_p2c[1]);
+
+    int status;
+    waitpid(pid, &status, 0); // Wait for the compile to end.
+    if (!WIFEXITED(status)) {
+      return "Compilation went wrong.. :(";
+    }
+  }
+}
 
 void RemoteExecuter::CodeExecutionThread() {
   while (true) {
