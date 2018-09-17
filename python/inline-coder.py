1212f50df3434336d10a3405f7f3c983bf960f65
Jaebum Lee <kev0960@gmail.com>
2018-09-16 23:59:16 -0700

    Fix inline codifier

--- python/inline-coder.py
+++ python/inline-coder.py
@@ -1,7 +1,10 @@
 import os
 
-def handle (data):
+
+def handle(data):
+
   def check_line(line):
+
     def check_char(c):
       if 'a' <= c <= 'z' or 'A' <= c <= 'Z' or '0' <= c <= '9':
         return True
@@ -20,7 +23,8 @@ def handle (data):
 
     words = line.split(' ')
 
-    i = 0, inline_code_start = False
+    i = 0
+    inline_code_start = False
     while i < len(words):
       w = words[i]
       if not is_code_chunk(w):
@@ -38,44 +42,49 @@ def handle (data):
           inline_code_start = True
           words[i] = '`' + w
       i += 1
+
     if inline_code_start:
       words[-1] = words[-1] + '`'
 
     annotated = ' '.join(words)
     while i < len(annotated):
-        if annotated[i] == '`':
-          end = annotated.find('`', i + 1)
-          w = annotated[i + 1:end]
-          if w in {'C', 'C++', 'Psi', 'C++ 11'}:
-            annotated = annotated[:i - 1] + ' ' + w + annotated[end + 1:]
-          elif w.isdigit() or (w.startswith('-') and w[1:].isdigit()):
-            annotated = annotated[:i - 1] + ' ' + w + annotated[end + 1:]
-        i += 1
+      if annotated[i] == '`':
+        end = annotated.find('`', i + 1)
+        w = annotated[i + 1:end]
+        if w in {'C', 'C++', 'Psi', 'C++ 11'}:
+          annotated = annotated[:i - 1] + ' ' + w + annotated[end + 1:]
+        elif w.isdigit() or (w.startswith('-') and w[1:].isdigit()):
+          annotated = annotated[:i - 1] + ' ' + w + annotated[end + 1:]
+      i += 1
 
     return annotated
 
   lines = data.split('\n')
 
-  fixed_lines = lines
+  fixed_lines = []
   in_code = False
 
-  while line in lines:
+  cnt = 0
+  for line in lines:
+    print(cnt, "line : ", line)
+    cnt += 1
     if line[:3] == '```':
-      in_code = !in_code
-      fixed_lines.push(line)
+      in_code = not in_code
+      fixed_lines.append(line)
       continue
 
-    if in_code:
-      fixed_lines.push(check_line(line))
-      
+    if not in_code:
+      fixed_lines.append(check_line(line))
+    else:
+      fixed_lines.append(line)
 
   return '\n'.join(fixed_lines)
-      
+
 
 if __name__ == "__main__":
   for filename in os.listdir("../md"):
     real_filename = "../md/" + filename
     with open(real_filename, "r+") as f:
       data = f.read()
-      f.seekg(0)
+      f.seek(0)
       f.write(handle(data))
